b67cf0f8c78b3a4682604c7b3c377902ddba9bf2
WIN11\Ignacio <ijoakin@gmail.com>
2026-08-31 22:46:39 -0300

    main should use application

--- src/application.h
+++ src/application.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include "ember.h"
 
 class Application
 {
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include "ember.h"
+#include "application.h"
 
 int main()
 {
 
-  Ember ember;
-  std::cout
-      << "Emeber v 1.0" << std::endl;
+  Application application;
+
+  application.Initialization();
+
+  application.Execution();
+
+  application.Shutdown();
+  std::cout << "Emeber v 1.0" << std::endl;
   return 0;
 }
