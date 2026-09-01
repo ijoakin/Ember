844a54bca094306434f186193cd0b0b8aa1b3644
WIN11\Ignacio <ijoakin@gmail.com>
2026-08-31 22:26:21 -0300

    add Ember class

--- src/ember.cxx
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main()
-{
-  std::cout << "Emeber v 1.0" << std::endl;
-  return 0;
-}
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "ember.h"
+
+int main()
+{
+
+  Ember ember;
+  std::cout
+      << "Emeber v 1.0" << std::endl;
+  return 0;
+}
