46f3331897e1befbba9ff1a1954d5716c5df7678
WIN11\Ignacio <ijoakin@gmail.com>
2026-08-31 22:43:38 -0300

    add application class

---
+++ src/application.cpp
@@ -0,0 +1,27 @@
+
+#include "application.h"
+
+Application::Application()
+{
+  std::cout << "Constructor" << std::endl;
+}
+
+void Application::Initialization()
+{
+  std::cout << "Initialization" << std::endl;
+}
+
+void Application::Execution()
+{
+  std::cout << "Execution" << std::endl;
+}
+
+void Application::Shutdown()
+{
+  std::cout << "Shutdown" << std::endl;
+}
+
+Application::~Application()
+{
+  std::cout << "Destructor" << std::endl;
+}
---
+++ src/application.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+
+class Application
+{
+
+public:
+  Application();
+  ~Application();
+  void Initialization();
+  void Execution();
+  void Shutdown();
+};
